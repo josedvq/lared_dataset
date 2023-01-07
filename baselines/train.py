06dd71144b8c3bcec3c5c80cb11f07a496a64b67
Jose Vargas <josedvq@gmail.com>
2023-01-06 22:44:25 -0600

    refactored

--- baselines/train.py
+++ baselines/train.py
@@ -5,9 +5,20 @@ import torch
 import torch.nn.functional as F
 import pytorch_lightning as pl
 from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import KFold, GroupKFold
 from torch.utils.data.dataloader import DataLoader
 from torch.utils.data.sampler import BatchSampler, RandomSampler, SequentialSampler
+from IPython.display import clear_output
 
+from lared_dataset.data_loading.dataset import FatherDataset, FatherDatasetSubset, RemoveConfidence
+from lared_dataset.data_loading.extractors import VideoExtractor, AccelExtractor
+from lared_dataset.baselines.video.models import make_slow_pretrained_body as get_video_feature_extractor
+from lared_dataset.baselines.video.transforms import get_kinetics_val_transform
+from lared_dataset.constants import (
+    processed_data_path,
+    processed_accel_path,
+    processed_videos_path,
+    examples_path)
 from .model import SegmentationFusionModel
 
 class System(pl.LightningModule):
@@ -83,7 +94,7 @@ def _collate_fn(batch):
 def train(i, train_ds, val_ds, modalities, 
         trainer_params={}, prefix=None, task='classification', 
         deterministic=False, eval_every_epoch=False, weights_path=None,
-        batch_size=32, num_workers=8):
+          batch_size=32, num_workers=8, enable_progress_bar=True):
 
     num_epochs = {
         ('accel',): 10,
@@ -130,7 +141,8 @@ def train(i, train_ds, val_ds, modalities,
             save_dir='logs/', name='', 
             version=prefix),
         deterministic=deterministic,
-        enable_checkpointing=False)
+        enable_checkpointing=False,
+        enable_progress_bar=enable_progress_bar)
         
     trainer.fit(system, data_loader_train, data_loader_val)
 
@@ -139,7 +151,8 @@ def train(i, train_ds, val_ds, modalities,
     
     return trainer #system.test_results
 
-def test(i, model, test_ds, prefix=None, batch_size=32):
+
+def test(i, model, test_ds, prefix=None, batch_size=32, enable_progress_bar=True):
 
     # data loaders
     g = torch.Generator()
@@ -157,11 +170,105 @@ def test(i, model, test_ds, prefix=None, batch_size=32):
 
     trainer = pl.Trainer(
                 logger= pl.loggers.TensorBoardLogger(
-                    save_dir='logs/', name='', 
-                    version=prefix))
+                save_dir='logs/', name='', 
+                version=prefix),
+                enable_progress_bar=enable_progress_bar)
 
     trainer.test(
         model=model, 
         dataloaders=test_dl)
     
     return trainer.model.test_results
+
+
+def do_cross_validation(do_train, ds, input_modalities, seed, 
+                        prefix=None, deterministic=False, enable_progress_bar=True):
+
+    cv_splits = list(GroupKFold(n_splits=3).split(
+        range(len(ds)), groups=ds.get_groups()))
+
+    all_results = []
+    for f, (train_idx, test_idx) in enumerate(cv_splits):
+        # if f == 0 or f == 1:
+        #     continue
+        # load feature caches for fold f
+        train_ds = FatherDatasetSubset(ds, train_idx, eval=False)
+        test_ds = FatherDatasetSubset(ds, test_idx, eval=True)
+
+        weights_path = os.path.join(
+            'weights',
+            f'I{"-".join(input_modalities)}_fold{f}.ckpt'
+        )
+
+        pl.utilities.seed.seed_everything(seed + f + 734890573)
+        batch_size = 8
+        if do_train:
+            trainer = train(f, train_ds, test_ds, input_modalities,
+                            prefix=prefix + f'_fold{f}' if prefix else None,
+                            eval_every_epoch=True,
+                            deterministic=deterministic,
+                            weights_path=weights_path,
+                            batch_size=batch_size,
+                            num_workers=4,
+                            enable_progress_bar=enable_progress_bar)
+            model = trainer.model
+        else:
+            model = System.load_from_checkpoint(checkpoint_path=weights_path)
+
+        # ensures that the testing is reproducible regardless of training
+        pl.utilities.seed.seed_everything(seed + f + 2980374334)
+        fold_outputs = test(f, model, test_ds,
+                            prefix=prefix + f'_fold{f}' if prefix else None,
+                            batch_size=8,
+                            enable_progress_bar=enable_progress_bar)
+        all_results.append(fold_outputs)
+        clear_output(wait=False)
+
+    outputs = [r['proba'].numpy() for r in all_results]
+    indices = [r['index'].numpy() for r in all_results]
+    metrics = [r['metric'] for r in all_results]
+    return metrics, outputs, indices
+
+
+def do_run(examples, input_modalities, do_train=True, 
+           deterministic=True, prefix='', enable_progress_bar=True):
+    ''' train = True will train the models, and requires 
+            model_label_modality = test_label_modality
+        train = False will load weights to test the models and does not require
+            model_label_modality = test_label_modality
+    '''
+    print(f'Using {len(examples)} examples')
+
+    # create the feature datasets
+    extractors = {}
+    if 'video' in input_modalities:
+        # os.path.join(processed_videos_path, 'examples')
+        videos_path = processed_videos_path
+        extractors['video'] = VideoExtractor(
+            videos_path, transform=get_kinetics_val_transform(8, 256, False))
+
+    if 'accel' in input_modalities:
+        accel_ds_path = os.path.join(
+            processed_accel_path, 'subj_accel_interp.pkl')
+        extractors['accel'] = AccelExtractor(accel_ds_path)
+
+    ds = FatherDataset(examples, extractors, transform=RemoveConfidence())
+
+    seed = 22
+    metrics, probas, indices = do_cross_validation(
+        do_train,
+        ds,
+        input_modalities=input_modalities,
+        deterministic=deterministic,
+        seed=seed,
+        prefix=f'{prefix}I{"-".join(input_modalities)}',
+        enable_progress_bar=enable_progress_bar)
+
+    torch.cuda.empty_cache()
+
+    return {
+        'metrics': metrics,
+        'probas': probas,
+        'indices': indices,
+        'seed': seed
+    }
--- baselines/train_cluster.py
+++ baselines/train_cluster.py
@@ -7,108 +7,14 @@ import pickle
 import traceback
 
 import torch
-
-import numpy as np
-import pandas as pd
-from sklearn.model_selection import KFold, GroupKFold
-from IPython.display import clear_output
+torch.backends.cudnn.benchmark = False
+torch.backends.cudnn.deterministic = False
 import pytorch_lightning as pl
 logging.getLogger("pytorch_lightning").setLevel(logging.WARNING)
-# set the cudnn
-torch.backends.cudnn.benchmark=False
-torch.backends.cudnn.deterministic=True
 
-from lared_dataset.data_loading.dataset import FatherDataset, FatherDatasetSubset, RemoveConfidence
-from lared_dataset.data_loading.extractors import VideoExtractor, AccelExtractor
-from lared_dataset.baselines.video.models import make_slow_pretrained_body as get_video_feature_extractor
-from lared_dataset.baselines.video.transforms import get_kinetics_val_transform
-from lared_dataset.constants import (
-    processed_data_path, 
-    processed_accel_path,
-    processed_videos_path,
-    examples_path)
-from lared_dataset.baselines.train import System, train, test
+from lared_dataset.constants import (examples_path)
+from lared_dataset.baselines.train import do_run
 
-def do_cross_validation(do_train, ds, input_modalities, seed, prefix=None, deterministic=False):
-    
-    cv_splits = list(GroupKFold(n_splits=3).split(range(len(ds)), groups=ds.get_groups()))
-    
-    all_results = []
-    for f, (train_idx, test_idx) in enumerate(cv_splits):
-        # if f == 0 or f == 1:
-        #     continue
-        # load feature caches for fold f
-        train_ds = FatherDatasetSubset(ds, train_idx, eval=False)
-        test_ds = FatherDatasetSubset(ds, test_idx, eval=True)
-
-        weights_path = os.path.join(
-            'weights',
-            f'I{"-".join(input_modalities)}_fold{f}.ckpt'
-        )
-
-        pl.utilities.seed.seed_everything(seed+f+734890573)
-        batch_size = 8
-        if do_train:
-            trainer = train(f, train_ds, test_ds, input_modalities,
-                prefix = prefix + f'_fold{f}' if prefix else None,
-                eval_every_epoch=True,
-                deterministic=deterministic,
-                weights_path=weights_path,
-                batch_size=batch_size,
-                num_workers=4)
-            model = trainer.model
-        else:
-            model = System.load_from_checkpoint(checkpoint_path=weights_path)
-
-        # ensures that the testing is reproducible regardless of training
-        pl.utilities.seed.seed_everything(seed+f+2980374334)
-        fold_outputs = test(f, model, test_ds, prefix = prefix + f'_fold{f}' if prefix else None, batch_size=8)
-        all_results.append(fold_outputs)
-        clear_output(wait=False)
-
-    outputs = [r['proba'].numpy() for r in all_results]
-    indices = [r['index'].numpy() for r in all_results]
-    metrics = [r['metric'] for r in all_results]
-    return metrics, outputs, indices
-
-def do_run(examples, input_modalities, 
-    do_train=True, deterministic=True, prefix=''):
-    ''' train = True will train the models, and requires 
-            model_label_modality = test_label_modality
-        train = False will load weights to test the models and does not require
-            model_label_modality = test_label_modality
-    '''
-    print(f'Using {len(examples)} examples')
-
-    # create the feature datasets
-    extractors = {}
-    if 'video' in input_modalities:
-        videos_path = processed_videos_path # os.path.join(processed_videos_path, 'examples')
-        extractors['video'] = VideoExtractor(videos_path, transform=get_kinetics_val_transform(8, 256, False))
-
-    if 'accel' in input_modalities:
-        accel_ds_path = os.path.join(processed_accel_path, 'subj_accel_interp.pkl')
-        extractors['accel'] = AccelExtractor(accel_ds_path)
-
-    ds = FatherDataset(examples, extractors, transform=RemoveConfidence())
-
-    seed = 22    
-    metrics, probas, indices = do_cross_validation(
-        do_train,
-        ds,
-        input_modalities=input_modalities,
-        deterministic=deterministic,
-        seed=seed,
-        prefix=f'{prefix}I{"-".join(input_modalities)}')
-
-    torch.cuda.empty_cache()
-
-    return {
-        'metrics': metrics,
-        'probas': probas,
-        'indices': indices,
-        'seed': seed
-    }
 
 def get_table(do_train=True, deterministic=True):
     examples = pickle.load(open(examples_path, 'rb'))
@@ -127,7 +33,8 @@ def get_table(do_train=True, deterministic=True):
             examples, 
             input_modalities, 
             do_train=do_train,
-            deterministic=deterministic)
+            deterministic=deterministic,
+            enable_progress_bar=False)
 
         res['-'.join(input_modalities)] = run_results
     return res
